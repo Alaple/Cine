062251a517589f1a5ebbdefcf4bb7aa18dcf41cc
Manuel Francisco Alonso <manuel1389@hotmail.com>
2021-06-05 20:02:37 -0300

    add Express and dependencies. getPeliculas backend

---
+++ data/peliculadb.js
@@ -0,0 +1,14 @@
+const connection = require('./connection');
+let objectId = require('mongodb').ObjectId;
+
+async function getPeliculas(){
+    const clientmongo = await connection.getConnection();
+    console.log(clientmongo);
+    const peliculas = await clientmongo.db('cine_tp2')
+        .collection('peliculas')
+        .find()
+        .toArray();
+    return peliculas;
+}
+
+module.exports = {getPeliculas};
