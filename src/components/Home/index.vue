5830c5862409bea1863e7a8ad271abc4f174332b
Alaple <lucaspalac94@hotmail.com>
2021-06-06 22:04:15 -0300

    Add Home and SelectMovie page
    We worked on front-end

---
+++ src/components/Home/index.vue
@@ -0,0 +1,4 @@
+<template src="./src/home.html"></template>
+<script src="./src/home.js"></script>
+<style src="./src/home.css" scoped lang="css"></style>
+
--- src/router.js
+++ src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-/* import Home from './components/Home/index.vue' */
+import Home from './components/Home/index.vue'
 import Login from './components/Login.vue'
+import PeliculaSeleccionada from './components/PeliculaSeleccionada/index.vue'
 /* import Register from './components/Register.vue'
 import MiPerfil from './components/MiPerfil.vue'
-import PeliculaSeleccionada from './components/PeliculaSeleccionada.vue'
 import PeliculaFunciones from './components/PeliculaFunciones.vue'
 import Peliculas from './components/Peliculas.vue'
 import Salas from './components/Salas.vue'
@@ -18,11 +18,11 @@ export const router = new VueRouter({
     mode: 'history',
     routes : [
         { path: '/', redirect:'/home' },
-/*         { path: '/home', component: Home }, */
+        { path: '/home', component: Home }, 
         { path: '/login', component: Login },
-/*         { path: '/register', component: Register },
-        { path: '/miPerfil', component: MiPerfil },
         { path: '/peliculaSeleccionada', component: PeliculaSeleccionada },
+/*          { path: '/register', component: Register },
+        { path: '/miPerfil', component: MiPerfil },
         { path: '/peliculaFunciones', component: PeliculaFunciones },
         { path: '/peliculas', component: Peliculas },
         { path: '/salas', component: Salas },
