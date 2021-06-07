7968e3b25a594fb4492a3e00269c7a54a5f16545
shape-shifters <professional.shapeshifters@gmail.com>
2021-06-07 12:35:42 -0300

    peliculas back added

--- data/peliculadb.js
+++ data/peliculadb.js
@@ -1,14 +1,74 @@
 const connection = require('./connection');
-//let objectId = require('mongodb').ObjectId;
+let objectId = require('mongodb').ObjectId;
+
+const dbName = 'cine_tp2';
+const collection = 'peliculas';
 
 async function getPeliculas(){
     const clientmongo = await connection.getConnection();
-    console.log(clientmongo);
-    const peliculas = await clientmongo.db('cine_tp2')
-        .collection('peliculas')
+    //console.log(clientmongo);
+    const peliculas = await clientmongo
+        .db(dbName)
+        .collection(collection)
         .find()
         .toArray();
     return peliculas;
 }
 
-module.exports = {getPeliculas};
+async function getPelicula(id) {
+    const clientMongo = await connection.getConnection();    
+    const pelicula = await clientMongo
+        .db(dbName)
+        .collection(collection)
+        .findOne({_id: new objectId(id)});
+    return pelicula;
+}
+
+async function addPelicula(pelicula) {
+    const clientMongo = await connection.getConnection();    
+    const result = await clientMongo
+        .db(dbName)
+        .collection(collection)
+        .insertOne(pelicula);
+    return result;
+}
+
+async function updatePelicula(pelicula) {
+    const clientMongo = await connection.getConnection();    
+    const query = {_id: new objectId(pelicula._id)};
+    const newValues = { $set: {
+            nombre: pelicula.nombre,
+            descripcion: pelicula.descripcion,
+            genero: pelicula.genero,
+            url_imagen: pelicula.url_imagen,
+            url_trailer: pelicula.url_trailer,
+            duracion: pelicula.duracion,
+            calificacion: pelicula.calificacion,
+            director: pelicula.director,
+            actores: pelicula.actores
+        }
+    };
+    const result = await clientMongo
+        .db(dbName)
+        .collection(collection)
+        .updateOne(query, newValues);
+    if (result.result.n != 1) {
+        throw new Error('[!] No ha sido posible actualizar la película');
+    }
+    return result;        
+}
+
+async function deletePelicula(id) {
+    const clientMongo = await connection.getConnection();    
+    const result = await clientMongo
+        .db(dbName)
+        .collection(collection)
+        .deleteOne({_id: new objectId(id)});
+    if (result.deletedCount != 1) {
+        throw new Error('[!] No ha sido posible eliminar la película');
+    }
+    return result;
+}
+
+
+module.exports = {getPeliculas, getPelicula, addPelicula, updatePelicula, deletePelicula};
--- routes/peliculas.js
+++ routes/peliculas.js
@@ -4,9 +4,80 @@ const dataPelicula = require('../data/peliculadb');
 const joi = require('joi');
 
 // /api/peliculas/
+
+/* GET Películas */
 router.get('/', async function(req, res, next) {
     let peliculas = await dataPelicula.getPeliculas();    
     res.json(peliculas);
 });
+
+/* GET película por id */
+router.get('/:id', async (req, res) => {
+    const pelicula = await dataPelicula.getPelicula(req.params.id);
+    if (pelicula) {
+        res.json(pelicula);
+    } else {
+        res.status(404).send('[!] Película no encontrada');
+    }
+});
+
+/* POST película */
+router.post('/', async (req, res) => {
+    const schema = joi.object({
+        nombre: joi.string().required(),
+        descripcion: joi.string().required(),
+        genero: joi.array().items(joi.string()).required(),
+        url_imagen: joi.string().required(),
+        url_trailer: joi.string().required(),
+        duracion: joi.number().integer().min(1).required(),
+        calificacion: joi.string().required(),
+        director: joi.string().required(),
+        actores: joi.array().items(joi.string()).required()
+    });
+    const result = schema.validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+    } else {
+        let pelicula = req.body;
+        pelicula = await dataPelicula.addPelicula(pelicula);
+        res.json(pelicula);
+    }
+});
+
+/* PUT película por id */
+router.put('/:id', async (req, res) => {
+    const schema = joi.object({
+        nombre: joi.string().required(),
+        descripcion: joi.string().required(),
+        genero: joi.array().items(joi.string()).required(),
+        url_imagen: joi.string().required(),
+        url_trailer: joi.string().required(),
+        duracion: joi.number().integer().min(1).required(),
+        calificacion: joi.string().required(),
+        director: joi.string().required(),
+        actores: joi.array().items(joi.string()).required()
+    });
+    const result = schema.validate(req.body);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message)
+    } else {
+        let pelicula = req.body;
+        pelicula._id = req.params.id;
+        pelicula = await dataPelicula.updatePelicula(pelicula);
+        res.json(pelicula);
+    }
+});
+
+/* DELETE pelicula por id */
+router.delete('/:id', async (req, res) => {
+    const pelicula = await dataPelicula.getPelicula(req.params.id)
+    if (pelicula){
+        dataPelicula.deletePelicula(req.params.id);
+        res.status(200).send('[!] Película eliminada');
+    } else {
+        res.status(404).send('[!] Película no encontrada');
+    }
+});
+
 
 module.exports = router;
